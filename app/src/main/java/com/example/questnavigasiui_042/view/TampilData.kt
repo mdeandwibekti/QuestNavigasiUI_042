8df6f0fa7fd7b05be0ed0430ae7b2d94665f32ad
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-07 16:20:24 +0700

    membuat colum untuk seperti Nama Lengkap dll

--- app/src/main/java/com/example/questnavigasiui_042/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_042/view/TampilData.kt
@@ -1,6 +1,14 @@
 package com.example.questnavigasiui_042.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -9,7 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_042.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,7 +42,9 @@ fun TampilanData(
                 title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
             )
-        }){
+        }){ isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+
     }
-    )
 }
