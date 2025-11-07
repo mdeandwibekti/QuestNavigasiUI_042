9e7028197baf68cede7d71dbd04820560ad06728
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-07 15:34:26 +0700

    membuat colum berisi textfield untuk nama lengkap

--- app/src/main/java/com/example/questnavigasiui_042/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_042/view/Formulir.kt
@@ -41,6 +41,22 @@ fun FormIsian(
                 )
             )
         }
-    )
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
+
+        }
     }
 }
